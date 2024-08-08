2a0943f34054169b8be8af8fbdc40f8f0c0653f3
tomguillermou <tomguillermou@gmail.com>
2024-08-08 11:43:14 +0200

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import 'dotenv/config';
+import 'dotenv/config'
+
+console.log('Testing a functionnality')
 
 export function main(): void {
-  console.log(`Env: ${process.env.NODE_ENV}`);
+  console.log(`Env: ${process.env.NODE_ENV}`)
 }
 
-main();
+main()
